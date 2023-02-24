68d59eb55d544642efb27d8c0fa38588a6d84a6f
kubakosiba <kubakosiba@localhost>
2023-02-24 12:46:00 -0500

    Comments for edits

---
+++ src/Jacob/java.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Jacob;
+
+/**
+ * Program takes user input for their name,
+ * attachs it to imput1 
+ * does the same for imput2 with generes
+ * then creates a new user profile and displays the info
+ *
+ * @author Jacob kosiba
+ */
+public class java {
+    
+}
